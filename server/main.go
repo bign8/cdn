048e4157227a422ae37f36a77d16fb9b7df74f4a
Nate Woods <big.nate.w@gmail.com>
2017-02-21 00:35:31 -0700

    Removing hackery for CDN configuration

--- server/main.go
+++ server/main.go
@@ -13,6 +13,7 @@ import (
 var (
 	target = flag.String("target", os.Getenv("TARGET"), "target hostname")
 	port   = flag.Int("port", 8081, "What port to run server on")
+	cap    = flag.Int("cap", 10, "How many requests to store in cache")
 )
 
 func check(err error) {
@@ -21,11 +22,13 @@ func check(err error) {
 	}
 }
 
-type cdn struct{}
+type cdn struct {
+	cap int
+}
 
 func (c *cdn) RoundTrip(req *http.Request) (*http.Response, error) {
 	log.Println("Proxying!", req.URL.String())
-	return nil, http.ErrSkipAltProtocol
+	return http.DefaultTransport.RoundTrip(req)
 }
 
 func main() {
@@ -33,13 +36,9 @@ func main() {
 	uri, err := url.Parse(*target)
 	check(err)
 	rp := httputil.NewSingleHostReverseProxy(uri)
-	CDN := &cdn{}
-
-	// Some hackery to create a man-in-the middle proxy
-	tp := http.DefaultTransport.(*http.Transport)
-	tp.RegisterProtocol("http", CDN)
-	rp.Transport = tp
-
+	rp.Transport = &cdn{
+		cap: *cap,
+	}
 	http.Handle("/", rp)
 	log.Printf("ReverseProxy for %q serving on :%d\n", *target, *port)
 	check(http.ListenAndServe(":"+strconv.Itoa(*port), nil))
