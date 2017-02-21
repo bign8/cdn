11242cad120ae0dc167cf35cbd565a43ea74c866
Nate Woods <big.nate.w@gmail.com>
2017-02-20 21:58:20 -0700

    Getting hacked reverse proxy together

--- server/main.go
+++ server/main.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"net/http"
+	"net/http/httputil"
+	"net/url"
+	"os"
+	"strconv"
+)
+
+var (
+	target = flag.String("target", os.Getenv("TARGET"), "target hostname")
+	port   = flag.Int("port", 8081, "What port to run server on")
+)
+
+func check(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
 
 func main() {
-	fmt.Println("TODO")
+	uri, err := url.Parse(*target)
+	check(err)
+	rp := httputil.NewSingleHostReverseProxy(uri)
+	http.Handle("/", rp)
+	fmt.Printf("ReverseProxy for %q serving on :%d\n", *target, *port)
+	check(http.ListenAndServe(":"+strconv.Itoa(*port), nil))
 }
