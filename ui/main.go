c502e9e449de77bffff6e21a707f90bf089c7096
Nate Woods <big.nate.w@gmail.com>
2017-02-26 10:52:06 -0700

    Stubbing out desire for UI

---
+++ ui/main.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Should listen to docker port mappings to ping each container directly
+	// Also should have some sweet gopherjs transpiled stockets stuff for status updates
+	fmt.Println("TODO: build a sweet UI that pulls vars and container healthchecks")
+}
