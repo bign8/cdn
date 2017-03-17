d39f1b44cb4e9cbba0aa41a1aa34258cd20cbbc1
Nate Woods <big.nate.w@gmail.com>
2017-03-17 10:45:29 -0600

    Starting to stub out charts

--- ui/src/js.js
+++ ui/src/js.js
@@ -10,6 +10,11 @@ function start() {
   ws.onmessage = function(event) {
     var m = JSON.parse(event.data);
     console.debug("Received message", m.typ);
+    myChart.data.datasets[0].data.push({
+      x: Date.now(),
+      y: 99,
+    });
+    myChart.update();
   }
 
   ws.onerror = function(event) { console.debug(event); };
@@ -33,3 +38,64 @@ function ping() {
 }
 
 start();
+
+var data = {
+    datasets: [
+        {
+            label: "Type 1 (99)",
+            fill: false,
+            borderColor: "rgba(75,192,192,1)",
+            data: [{
+              x: Date.now() - 2 * 1000,
+              y: 99,
+            }, {
+              x: Date.now()- 1000,
+              y: 80,
+            }, {
+              x: Date.now(),
+              y: 81,
+            }],
+        }, {
+          label: "Type 2 (23)",
+          fill: false,
+          data: [{
+            x: Date.now(),
+            y: 62,
+          }, {
+            x: Date.now() + 1000,
+            y: 68,
+          }, {
+            x: Date.now() + 2 * 1000,
+            y: 99,
+          }],
+        }
+    ]
+};
+
+var options = {
+  scales: {
+    xAxes: [{
+      type: "time",
+      time: {
+        unit: 'second',
+      },
+    }]
+  },
+  tooltips: {
+    enabled: false,
+  //   callbacks: {
+  //     title: function(items, data) {
+  //       // TODO: convert xlabel to pretty string
+  //       var obj = items[0];
+  //       return data.datasets[obj.datasetIndex].data[obj.index].x;
+  //     },
+  //   }
+  }
+};
+
+var chart = document.getElementById('chart');
+var myChart = new Chart(chart, {
+  type: 'line',
+  data: data,
+  options: options
+});
