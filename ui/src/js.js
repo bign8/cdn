cd4aca69aa93a86b7351240832dd40b75deb24ca
Nate Woods <big.nate.w@gmail.com>
2017-03-18 19:19:34 -0600

    Starting to build brain stat object

--- ui/src/js.js
+++ ui/src/js.js
@@ -1,6 +1,6 @@
 // Chart documentation: http://www.chartjs.org/docs/
 
-var ws, ticker, framer, last;
+var ws, ticker, framer, last, brains;
 
 function start() {
   ws = new WebSocket("ws://" + window.location.host + "/ws/admin");
@@ -11,12 +11,11 @@ function start() {
 
   ws.onmessage = function(event) {
     var m = JSON.parse(event.data);
-    console.debug("Received message", m.typ);
-    myChart.data.datasets[0].data.push({
-      x: Date.now(),
-      y: 99,
-    });
-    myChart.update();
+    switch (m.typ) {
+      case 'stat': brain.addStat(m); break;
+      case 'pong': console.debug("Received PONG"); break;
+      default: console.debug("Received unknown message type", m.typ); break;
+    }
   }
 
   ws.onerror = function(event) { console.debug(event); };
@@ -41,69 +40,65 @@ function ping() {
 
 start();
 
-var data = {
-    datasets: [
-        {
-            label: "Type 1 (99)",
-            fill: false,
-            borderColor: "rgba(75,192,192,1)",
-            data: [{
-              x: Date.now() - 2 * 1000,
-              y: 99,
-            }, {
-              x: Date.now()- 1000,
-              y: 80,
-            }, {
-              x: Date.now(),
-              y: 81,
-            }],
-        }, {
-          label: "Type 2 (23)",
-          fill: false,
-          data: [{
-            x: Date.now(),
-            y: 62,
-          }, {
-            x: Date.now() + 1000,
-            y: 68,
-          }, {
-            x: Date.now() + 2 * 1000,
-            y: 99,
-          }],
-        }
-    ]
-};
-
-var options = {
-  responsive: false, // don't resize
-  scales: {
-    xAxes: [{
-      type: "time",
-      time: {
-        unit: 'second',
+function createChart(ctx, name) {
+  return new Chart(ctx, {
+    type: 'line',
+    data: {
+      datasets: [],
+    },
+    options: {
+      responsive: false, // don't resize
+      title: {
+        display: true,
+        text: name,
       },
-    }]
-  },
-  tooltips: {
-    enabled: false,
-  //   callbacks: {
-  //     title: function(items, data) {
-  //       // TODO: convert xlabel to pretty string
-  //       var obj = items[0];
-  //       return data.datasets[obj.datasetIndex].data[obj.index].x;
-  //     },
-  //   }
-  }
-};
+      scales: {
+        xAxes: [{
+          type: "time",
+          display: false,
+        }],
+        yAxes: [{
+          ticks: {
+            beginAtZero: true,
+          },
+          // scaleLabel: {
+          //   display: true,
+          //   labelString: "count",
+          // },
+        }],
+      },
+      tooltips: {
+        enabled: false,
+      //   callbacks: {
+      //     title: function(items, data) {
+      //       // TODO: convert xlabel to pretty string
+      //       var obj = items[0];
+      //       return data.datasets[obj.datasetIndex].data[obj.index].x;
+      //     },
+      //   }
+      }
+    }
+  })
+}
 
-var chart = document.getElementById('chart');
-var myChart = new Chart(chart, {
-  type: 'line',
-  data: data,
-  options: options
-});
+function createDataset(name, color) {
+  return {
+    label: name,
+    fill: false,
+    borderColor: color,
+    data: [],
+  };
+}
 
-// TODO: make all this smart
+var myChart = createChart(document.getElementById('chart'), 'Origin xxxx');
+myChart.data.datasets.push(createDataset('Type 1', 'rgba(75,192,192,1)'));
+myChart.data.datasets.push(createDataset('Type 2', ''));
+
+// Setup mock data
+var now = Date.now();
+myChart.data.datasets[0].data = [{x: now - 2 * 1000, y: 99}, {x: now - 1000, y: 80}, {x: now, y: 81}];
+myChart.data.datasets[1].data = [{x: now, y: 62}, {x: now + 1000, y: 68}, {x: now + 2 * 1000, y: 99}];
+myChart.update();
 
 function checkSetIdx(idx, now) {
   var obj = myChart.data.datasets[idx];
@@ -128,3 +123,34 @@ function setZeros() {
 }
 
 setZeros();
+
+function Brain() {
+  this.kinds = ['origin', 'server', 'client']; // TODO: allow re-ordering
+  this.charts = {}; // map of ids to chart types
+  this.stats = {}; // map of ids to stat metadata
+}
+
+Brain.prototype.addStat = function(stat) {
+  // {"typ":"stat","msg":{"img":{"tot":4,"new":1}},"who":{"id":"C417A89F4D8B2FA392595277A31A41B3","kind":"origin"}}
+  console.log(stat);
+
+  // Initalize chart object if necessary
+  if (!this.charts.hasOwnProperty(stat.who.id)) {
+    console.log('TODO: initalize chart');
+    var ctx = document.createElement('canvas');
+    ctx.width = 800;
+    ctx.height = 400;
+    document.body.appendChild(ctx); // TODO: figure out the correct place to put this chart
+    this.charts[stat.who.id] = createChart(ctx, stat.who.kind + " " + stat.who.id.substr(0, 10).toLowerCase());
+  } else {
+    console.log('chart initailized');
+  }
+  myChart.data.datasets[0].data.push({
+    x: Date.now(),
+    y: 99,
+  });
+  myChart.update();
+};
+
+// Initalize the brain
+brain = new Brain();
