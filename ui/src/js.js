b3f8169508c48727d838a4ab59c8715ccefd3669
Nate Woods <big.nate.w@gmail.com>
2017-03-19 00:02:34 -0600

    Moving setZeros to Brain and actuall setting stats objects

--- ui/src/js.js
+++ ui/src/js.js
@@ -1,6 +1,6 @@
 // Chart documentation: http://www.chartjs.org/docs/
 
-var ws, ticker, framer, last, brains;
+var ws, ticker, last, brains;
 
 function start() {
   ws = new WebSocket("ws://" + window.location.host + "/ws/admin");
@@ -22,20 +22,15 @@ function start() {
 
   ws.onclose = function(event) {
     console.debug("closing", event);
-    window.cancelAnimationFrame(framer);
     window.clearTimeout(ticker);
     last *= 2;
     window.setTimeout(start, Math.floor(Math.random() * last));
   }
 }
 
-function pong() {
-  ws.send(JSON.stringify({typ: "ping"}));
-  ticker = window.setTimeout(ping, 30 * 1000);
-}
-
 function ping() {
-  framer = window.requestAnimationFrame(pong);
+  ws.send(JSON.stringify({typ: "ping"}));
+  ticker = window.setTimeout(window.requestAnimationFrame.bind(window, ping), 30 * 1000);
 }
 
 start();
@@ -44,6 +39,7 @@ function createChart(ctx, name) {
   return new Chart(ctx, {
     type: 'line',
     data: {
+      metadata: {}, // Custom data map (stat-name -> dataset index)
       datasets: [],
     },
     options: {
@@ -90,67 +86,71 @@ function createDataset(name, color) {
   };
 }
 
-var myChart = createChart(document.getElementById('chart'), 'Origin xxxx');
-myChart.data.datasets.push(createDataset('Type 1', 'rgba(75,192,192,1)'));
-myChart.data.datasets.push(createDataset('Type 2', ''));
-
-// Setup mock data
-var now = Date.now();
-myChart.data.datasets[0].data = [{x: now - 2 * 1000, y: 99}, {x: now - 1000, y: 80}, {x: now, y: 81}];
-myChart.data.datasets[1].data = [{x: now, y: 62}, {x: now + 1000, y: 68}, {x: now + 2 * 1000, y: 99}];
-myChart.update();
-
-function checkSetIdx(idx, now) {
-  var obj = myChart.data.datasets[idx];
-  if (now - obj.data[obj.data.length - 1].x > 1500) {
-    obj.data.push({
-      x: now,
-      y: 0,
-    });
-  }
-  if (obj.data.length > 10) {
-    obj.data.shift();
-  }
-}
-
-function setZeros() {
-  console.log("setting zeros");
-  var now = Date.now();
-  checkSetIdx(0, now);
-  checkSetIdx(1, now);
-  myChart.update();
-  window.setTimeout(window.requestAnimationFrame.bind(this, setZeros), 1000);
-}
-
-setZeros();
-
 function Brain() {
+  this.colors = ['rgba(75,192,192,1)'];
   this.kinds = ['origin', 'server', 'client']; // TODO: allow re-ordering
-  this.charts = {}; // map of ids to chart types
-  this.stats = {}; // map of ids to stat metadata
+  this.charts = {}; // map of ids to chart types (stat.who.id -> chart)
 }
 
 Brain.prototype.addStat = function(stat) {
   // {"typ":"stat","msg":{"img":{"tot":4,"new":1}},"who":{"id":"C417A89F4D8B2FA392595277A31A41B3","kind":"origin"}}
   console.log(stat);
+  var now = Date.now();
 
   // Initalize chart object if necessary
   if (!this.charts.hasOwnProperty(stat.who.id)) {
-    console.log('TODO: initalize chart');
     var ctx = document.createElement('canvas');
     ctx.width = 800;
     ctx.height = 400;
     document.body.appendChild(ctx); // TODO: figure out the correct place to put this chart
     this.charts[stat.who.id] = createChart(ctx, stat.who.kind + " " + stat.who.id.substr(0, 10).toLowerCase());
-  } else {
-    console.log('chart initailized');
   }
-  myChart.data.datasets[0].data.push({
-    x: Date.now(),
-    y: 99,
-  });
-  myChart.update();
+  var active = this.charts[stat.who.id];
+
+  // Iterate the stat object
+  for (var key in stat.msg) {
+
+    // We do not have an index for this statistic (create one)
+    if (!active.data.metadata.hasOwnProperty(key)) {
+      active.data.metadata[key] = Object.keys(active.data.metadata).length
+      active.data.datasets.push(createDataset(key, "")); // TODO: generate color
+    }
+
+    // TODO: change dataset name to have total included in suffix
+    active.data.datasets[active.data.metadata[key]].data.push({x: now, y: stat.msg[key].new});
+  }
+  active.update();
+};
+
+// For all charts, prune total data points and plot additional 0 points as time progresses
+Brain.prototype.setZeros = function() {
+  console.debug("Brain setting zeros");
+  var now = Date.now();
+
+  // For all charts
+  for (var cid in this.charts) {
+
+    // For all datasets
+    for (var i = 0, chart = this.charts[cid].data; i < chart.datasets.length; i++) {
+      var obj = chart.datasets[i];
+
+      // if the newest item is older than 1.5 seconds
+      if (now - obj.data[obj.data.length - 1].x > 1500) {
+        obj.data.push({x: now, y: 0}); // add dummy zero point
+      }
+
+      // if the length of data being shown is more than 10
+      if (obj.data.length > 10) {
+        obj.data.shift(); // prune one off
+      }
+    }
+    this.charts[cid].update(); // TODO: don't update if data hasn't changed
+  }
+
+  // Run this method again at a designated time in the future, ontop of an animation frame
+  window.setTimeout(window.requestAnimationFrame.bind(window, this.setZeros.bind(this)), 1000);
 };
 
 // Initalize the brain
 brain = new Brain();
+brain.setZeros();
